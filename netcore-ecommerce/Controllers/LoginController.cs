4dbeae88da29f42a530199f85b12f062efe67b80
JalilAbdullayev <abdullacelil00@gmail.com>
2024-08-27 06:46:53 +0400

    Create login & register views

---
+++ netcore-ecommerce/Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace netcore_ecommerce.Controllers {
+    public class LoginController: Controller {
+        // GET: LoginController
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ netcore-ecommerce/Controllers/RegisterController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace netcore_ecommerce.Controllers {
+    public class RegisterController: Controller {
+        // GET: RegisterController
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
